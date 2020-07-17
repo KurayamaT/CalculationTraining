dfaf2d4b313b03870d1a1bdc3f59788da85ca748
KurayamaT <t.kurayama@nifty.com>
2020-07-17 22:08:51 +0900

    試しにコメント付けただけ

--- app/src/main/java/net/minpro/calculationtraining/MainActivity.kt
+++ app/src/main/java/net/minpro/calculationtraining/MainActivity.kt
@@ -1,11 +1,29 @@
 package net.minpro.calculationtraining
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val arrayAdapter = ArrayAdapter.createFromResource(this, R.array.number_of_question, android.R.layout.simple_spinner_item)
+
+        spinner.adapter= arrayAdapter
+
+        button.setOnClickListener {
+            val numberOfQuestion = spinner.selectedItem.toString().toInt()
+
+            val intent = Intent(this@MainActivity, TestActivity::class.java)
+            intent.putExtra("numberOfQuestion", numberOfQuestion)
+
+
+        }
     }
 }
--- app/src/main/java/net/minpro/calculationtraining/TestActivity.kt
+++ app/src/main/java/net/minpro/calculationtraining/TestActivity.kt
@@ -2,10 +2,57 @@ package net.minpro.calculationtraining
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_test.*
 
-class TestActivity : AppCompatActivity() {
+
+class TestActivity : AppCompatActivity(), View.OnClickListener {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
+
+        val bundle = intent.extras
+        val numberOfQuestion = bundle?.getInt("numberOfQuestion")
+        textRemining.text = numberOfQuestion.toString()
+
+        Button_answerCheck.setOnClickListener {
+
+            answerCheck()
+        }
+
+        button_turnBack.setOnClickListener {  }
+
+        button0.setOnClickListener(this)
+        button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
+        button4.setOnClickListener(this)
+        button5.setOnClickListener(this)
+        button6.setOnClickListener(this)
+        button7.setOnClickListener(this)
+        button8.setOnClickListener(this)
+        button9.setOnClickListener(this)
+        buttonMinus.setOnClickListener(this)
+        buttonClear.setOnClickListener(this)
+
+        question()
+
+
     }
+
+    private fun question(){
+        TODO("Not yet implemented")
+    }
+
+    private fun answerCheck() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
+    }
+//ちょっとかえたぜ
+
 }
